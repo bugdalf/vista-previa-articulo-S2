7b0b41d33fd1a1ee8040c6ab8a3fc469dc4e9bdb
ankynator <ankynator@gmail.com>
2020-08-31 12:12:48 -0500

    Mejorando el box, y acomodando los tamaños

--- src/index.js
+++ src/index.js
@@ -5,19 +5,32 @@ const shareContainer = document.querySelector('#shareContainer')
 let state = false
 
 const position = () => {
+    const screenWidth = screen.width
+    const screenHeight = screen.height
+
     const x = shareContainer.offsetLeft
     const y = shareContainer.offsetTop
     const width = shareContainer.clientWidth
     const height = shareContainer.clientHeight
-    console.log(x, y)
-    
+    const buttonX = buttonShare.offsetLeft
+    const buttonY = buttonShare.offsetTop
+
+    console.log(buttonX, buttonY)
+
+    if(screenWidth > 768){
+        typeBoxShare(buttonX-133, buttonY-70, 300, 50)
+    } else {
+        typeBoxShare(x, y, width, height)
+    }
+}
+const typeBoxShare = (x, y, width, height) => {
     boxShare.style.left = `${x}px`
     boxShare.style.top = `${y}px`
 
     boxShare.style.width = `${width}px`
     boxShare.style.height = `${height}px`
-
 }
+
 
 buttonShare.addEventListener('click', (e) => {
     position()
